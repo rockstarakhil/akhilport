c1a48cef72b193d5bc7e769a0f4ddcc9b43c534d
rockstarakhil <b.akhil072@gmail.com>
2022-11-21 12:14:57 +0530

    new changes for removing login and log outshuh

--- dennisivy/settings.py
+++ dennisivy/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hrj48*mobkzo)s6q+2wfqcg=_=@5c38n&sb4_vlp#bb^zyp_pi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','akhilanalyst.herokuapp.com']
 
